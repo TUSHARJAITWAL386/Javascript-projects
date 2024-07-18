10d72bfaf33652fd4d588fed7fbd7f71becd7d50
TUSHARJAITWAL386 <tushar.j@webkorps.com>
2024-07-19 01:09:17 +0530

    Basic Javascript Program

---
+++ script.js
@@ -0,0 +1,19 @@
+//Single line comment
+// console.log("Hello JavaScript")
+// document.write("Ruby on Rails Developer")
+// console.warn("This is Warning!")
+// console.error("This is an error")
+// console.clear() your console is clear
+// console.assert(4==6) Assertion failed
+
+/*
+multi
+line
+comment
+*/
+
+
+var number1 = 50;
+var number2 = 50;
+
+console.log(number1 + number2);
